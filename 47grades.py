9b0d3e0e73b388bb11b6d278bbef965174d2772e
FMartinez2022 <97995180+FMartinez2022@users.noreply.github.com>
2022-02-01 20:45:21 -0500

    Add files via upload

---
+++ 47grades.py
@@ -0,0 +1,29 @@
+
+def computegrade(score):
+    if score > 1.0:
+        return ("Bad score")
+
+    elif score < 0.0:
+        return ("Bad score")
+
+    elif score >= 0.9:
+        return("A")
+
+    elif score >= 0.8:
+        return("B")
+
+    elif score >= 0.7:
+        return("C")
+
+    elif score >= 0.6:
+        return("D")
+
+    elif score <= 0.6:
+        return("F")
+
+try:
+    score = float(input("Enter Score: "))
+except:
+    print("Please enter numeric value")
+    exit()
+print("Your grade is: ", computegrade(score))
